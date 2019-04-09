57ddc1495f07197430d8f507b9cbe9a7a814b046
tebr5923 <tebr5923@mail.ru>
2019-04-09 20:17:38 +0700

    ArrayStorage implementation

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,76 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+        if (size != 0) {
+            for (int a = 0; a < size; a++) {
+                storage[a] = null;
+            }
+            size = 0;
+        }
     }
 
     void save(Resume r) {
+        boolean isExist = false;
+        if (r.uuid != null) {
+            if (size == 0) {
+                storage[size] = r;
+                size++;
+            } else {
+                // смотрим есть ли уже такой элeмент
+                for (int a = 0; a < size; a++) {
+                    if (storage[a].uuid == r.uuid) {
+                        isExist = true;
+                    }
+                }
+                if (isExist == false) {
+                    storage[size] = r;
+                    size++;
+                }
+
+            }
+
+        }
     }
 
     Resume get(String uuid) {
+        if (size != 0) {
+            for (int a = 0; a < size; a++) {
+                if (storage[a].uuid == uuid)
+                    return storage[a];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        if (size != 0) {
+            for (int a = 0; a < size; a++) {
+                if (storage[a].uuid == uuid) {
+                    for (int k = a; k < size; k++) {
+                        storage[k] = storage[k + 1];
+                    }
+                    size--;
+                }
+            }
+        }
     }
+
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
